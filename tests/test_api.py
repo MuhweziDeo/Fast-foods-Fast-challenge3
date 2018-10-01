c6f20a7237da78cfd39e585e46258d9c2cf2a1cb
muhwezideo <aggrey256@gmail.com>
2018-10-01 22:10:38 +0200

    [Delivers #160877071] add test for user registration endpoint

---
+++ tests/test_api.py
@@ -0,0 +1,27 @@
+import unittest
+import json
+import os
+from api.app import app
+from api.models.db import DB
+
+class APITest(unittest.TestCase):
+    def setUp(self):
+        self.app=app
+        self.dbconnector=DBconfig(host='localhost',user='postgres', dbname='',port=5432,password='sudo')
+
+        print(self.dbconnector.dbname)
+        self.dbconnector.drop_all_tables('orders','users','fastfoods')
+        self.dbconnector.create_db_tables()
+        self.client = self.app.test_client()
+        self.user = {
+            'username': 'dee',
+            'password': 'dee'
+        }
+     
+    def test_create_user(self):
+        res=self.client.post('/api/v2/users',
+                                    data=json.dumps(self.user),
+                                    content_type='application/json'
+                                    )
+        self.assertEqual(res.status_code, 200)
+        self.assertIn("user created",str(res.data))
