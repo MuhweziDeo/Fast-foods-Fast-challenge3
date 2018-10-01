f343e7021f78bd7998b1f88f249ce3605bda64bc
muhwezideo <aggrey256@gmail.com>
2018-10-02 00:59:44 +0200

    [Delivers #160877071] add database operation to register a user

--- api/models/db.py
+++ api/models/db.py
@@ -1,33 +1,34 @@
 import psycopg2
 from psycopg2.extras import RealDictCursor
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class DB():
-    def __init__(self, host, user, dbname, password):
-        try:
-            self.connection = psycopg2.connect(
+  def __init__(self, host, user, dbname, password):
+    try:
+      self.connection = psycopg2.connect(
 
-                host="localhost",
-                user="postgres",
-                dbname="fastfoodsapi",
-                password="sudo"
-            )
-            self.cur = self.connection.cursor()
-            print('connection succeful {}'.format(dbname))
-        except(Exception, psycopg2.DatabaseError) as e:
-            print(e)
+          host="localhost",
+          user="postgres",
+          dbname="fastfoodsapi",
+          password="sudo"
+      )
+      self.cur = self.connection.cursor()
+      print('connection succeful {}'.format(dbname))
+    except(Exception, psycopg2.DatabaseError) as e:
+      print(e)
 
-    def create_db_tables(self):
-        queries = (
+  def create_db_tables(self):
+    queries = (
 
-            """
+        """
             CREATE TABLE IF not EXISTS  fastfoods(
               meal_id serial ,
               meal_name varchar(20) not null primary key unique,
               price int not null
             );
             """,
-            """
+        """
             CREATE TABLE IF not EXISTS  users(
               user_id serial primary key,
               username varchar(25) not null unique,
@@ -37,7 +38,7 @@ class DB():
             );
             """,
 
-            """
+        """
             CREATE TABLE IF not EXISTS orders(
               orderid serial primary key,
               user_id int not null,
@@ -52,24 +53,46 @@ class DB():
             );
 
             """
-        )
-        try:
-            for query in queries:
-                self.cur.execute(query)
-                self.connection.commit()
-                print('Tables Created')
-        except(Exception, psycopg2.DatabaseError) as error:
-            print(error, "Dont Panic But  Table Creation Failed")
+    )
+    try:
+      for query in queries:
+        self.cur.execute(query)
+        self.connection.commit()
+        print('Tables Created')
+    except(Exception, psycopg2.DatabaseError) as error:
+      print(error, "Dont Panic But  Table Creation Failed")
 
-    def drop_all_tables(self, *tables):
-        try:
-            for table in tables:
-                query = 'DROP TABLE IF EXISTS {}'.format(table)
-                self.cur.execute(query)
-                print(query)
-                print("Deleted")
-        except(Exception, psycopg2.DatabaseError) as error:
-            print(error, 'Failed')
+  def drop_all_tables(self, *tables):
+    try:
+      for table in tables:
+        query = 'DROP TABLE IF EXISTS {}'.format(table)
+        self.cur.execute(query)
+        print(query)
+        print("Deleted")
+    except(Exception, psycopg2.DatabaseError) as error:
+      print(error, 'Failed')
+
+     # users
+  def hash_password(self, password):
+    return generate_password_hash(password)
+
+  def confirm_password_hash(self, password, pasword_hash):
+    return check_password_hash(pasword_hash, password)
+
+  def register_user(self, username, password):
+    try:
+      password = self.hash_password(password)
+      # new_user=User(username,password)
+      create_user = "INSERT INTO users(username,password) VALUES ('{}','{}')".format(
+                    username, password)
+      print(create_user)
+      self.cur.execute(create_user)
+      # print(new_user)
+      return {'message': 'user created'}
+    except(Exception, psycopg2.DatabaseError) as error:
+      print(error)
+      # return None
+      return {'message': 'Please choose a another name because username {} already exits'.format(username)}
 
 
 db = DB(host="localhost", user="postgres",
