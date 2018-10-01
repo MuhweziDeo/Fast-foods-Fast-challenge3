9a74af9e75c8901882ee8a48afd129fe6871ba64
muhwezideo <aggrey256@gmail.com>
2018-10-01 22:32:16 +0200

    refactor:fix typo errors

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,24 +4,26 @@ import os
 from api.app import app
 from api.models.db import DB
 
+
 class APITest(unittest.TestCase):
     def setUp(self):
-        self.app=app
-        self.dbconnector=DBconfig(host='localhost',user='postgres', dbname='',port=5432,password='sudo')
+        self.app = app
+        self.db = DB(host='localhost', user='postgres',
+                     dbname='', password='sudo')
 
         print(self.dbconnector.dbname)
-        self.dbconnector.drop_all_tables('orders','users','fastfoods')
-        self.dbconnector.create_db_tables()
+        self.db.drop_all_tables('orders', 'users', 'fastfoods')
+        self.db.create_db_tables()
         self.client = self.app.test_client()
         self.user = {
             'username': 'dee',
             'password': 'dee'
         }
-     
+
     def test_create_user(self):
-        res=self.client.post('/api/v2/auth/users',
-                                    data=json.dumps(self.user),
-                                    content_type='application/json'
-                                    )
+        res = self.client.post('/api/v2/auth/users',
+                               data=json.dumps(self.user),
+                               content_type='application/json'
+                               )
         self.assertEqual(res.status_code, 200)
-        self.assertIn("user created",str(res.data))
+        self.assertIn("user created", str(res.data))
--- tests/test_database.py
+++ tests/test_database.py
@@ -5,14 +5,14 @@ from api.models.db import DB
 class DBTestCase(unittest.TestCase):
     def setUp(self):
         self.db = DB(
-            host='localhost', user='postgres', dbname='', port=5432, password='sudo')
-        self.dbconnector.drop_all_tables('orders', 'users', 'fastfoods')
-        self.dbconnector.create_db_tables()
+            host='localhost', user='postgres', dbname='', password='sudo')
+        self.db.drop_all_tables('orders', 'users', 'fastfoods')
+        self.db.create_db_tables()
 
     def test_create_user(self):
-        query = self.dbconnector.register_user('dee', 'dee')
+        query = self.db.register_user('dee', 'dee')
         self.assertIn('user created', str(query))
 
     def tearDown(self):
-        self.dbconnector.drop_all_tables('orders', 'fastfoods', 'users',)
+        self.db.drop_all_tables('orders', 'fastfoods', 'users',)
         print('deleted')
