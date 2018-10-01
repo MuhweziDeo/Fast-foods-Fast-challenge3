f454efd40e61fde5f5bba73c85ccd237d49dbcb9
muhwezideo <aggrey256@gmail.com>
2018-10-01 22:23:19 +0200

    refactor:fix wrong password for database connection

--- tests/test_database.py
+++ tests/test_database.py
@@ -5,7 +5,7 @@ from api.models.db import DB
 class DBTestCase(unittest.TestCase):
     def setUp(self):
         self.db = DB(
-            host='localhost', user='postgres', dbname='', port=5432, password='Adeo256.')
+            host='localhost', user='postgres', dbname='', port=5432, password='sudo')
         self.dbconnector.drop_all_tables('orders', 'users', 'fastfoods')
         self.dbconnector.create_db_tables()
 
