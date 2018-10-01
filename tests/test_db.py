3e8ca1481683a0ec2d5ef9062ad470fff12e3231
muhwezideo <aggrey256@gmail.com>
2018-10-02 01:50:22 +0200

    [Delivers #160877071] add test for register user database operation

---
+++ tests/test_db.py
@@ -0,0 +1,14 @@
+import unittest
+from api.models.db import DB
+
+
+class DBTestCase(unittest.TestCase):
+    def setUp(self):
+        self.db = DB(host='localhost', user='postgres',
+                     dbname='', password='Adeo256.')
+        self.db.drop_all_tables('orders', 'users', 'fastfoods')
+        self.db.create_db_tables()
+
+    def test_create_user(self):
+        query = self.db.register_user('dee', 'dee')
+        self.assertIn('user created', str(query))
