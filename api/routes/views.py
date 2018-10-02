7040176d3892e7b702b7eba72ba8faf119aed1ff
muhwezideo <aggrey256@gmail.com>
2018-10-02 05:55:02 +0200

    [Delivers #160877145]add route to handle user login

--- api/routes/views.py
+++ api/routes/views.py
@@ -25,3 +25,21 @@ class Signup(Resource):
         username = data['username']
         password = data['password']
         return db.register_user(username, password)
+
+
+@api.route('auth/login')
+class Login(Resource):
+    @api.expect(user)
+    def post(self):
+        data = api.payload
+        username = data['username']
+        attempted_password = data['password']
+        user = db.find_by_username(username)
+        if user:
+            pasword_hash = user[2]
+            if db.confirm_password_hash(attempted_password, pasword_hash):
+                return {'message': 'You have been Verified'}
+            else:
+                return {'message': 'password verification failed'}
+        else:
+            return {'message': 'username {} deosnt exist'.format(username)}
